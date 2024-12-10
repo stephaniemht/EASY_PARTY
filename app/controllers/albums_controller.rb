ccf9c25013a2e917ca5e3ada2018f5c0a470aa87
nicolas belin <belin.nicolas89@gmail.com>
2024-12-10 14:37:03 +0100

    en attente pull

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,23 @@
 class AlbumsController < ApplicationController
+  before_action :set_event
+  before_action :set_album
+
+  def index
+    @album = @event.albums
+  end
+
+  def new
+    
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[event_id])
+  end
+  
+  def set_album
+    @album = Album.find(params[:id])
+  end
 
 end
