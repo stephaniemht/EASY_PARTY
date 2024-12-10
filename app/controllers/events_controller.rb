3ff3407a213ba273831fc1d114e8fbb97b7e5c69
stephanie mihut <stephanie.mihut@gmail.com>
2024-12-10 14:11:59 +0100

    view en cours de creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,48 +1,47 @@
 class EventsController < ApplicationController
-
-def index
-  @events = Event.all
-end
-
-def show
-  @event = Event.find(params[:id])
-end
-
-def new
-  @event = Event.new
-end
-
-def create
-  @event = Event.new(event_params)
-  if @event.save
-    redirect_to @event, notice: 'Evenement crée avec succès !'
-  else
-    render :new, status: unprocessable_entity
-end
-
-def edit
-  @event = Event.find(params[:id])
-end
-
-def update
-  @event = Event.find(params[:id])
-  if @event.update
-    redirect_to @event, notice: 'Evenement mis à jour avec succès !'
-  else
-    render :edit, status: unprocessable_entity
+  def index
+    @events = Event.all
   end
 
-  def destroy
+  def show
     @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to events_path, notice: 'Evenement supprimé avec succès !'
   end
 
-  private
+  def new
+    @event = Event.new
+  end
 
-  def event_params
-    params.require(:event).permit(:name, :address)
-end
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Evenement crée avec succès !'
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
 
+  def update
+    @event = Event.find(params[:id])
+    if @event.update
+      redirect_to @event, notice: 'Evenement mis à jour avec succès !'
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end
+
+    def destroy
+      @event = Event.find(params[:id])
+      @event.destroy
+      redirect_to events_path, notice: 'Evenement supprimé avec succès !'
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:name, :address)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,16 @@ Event.destroy_all
 User.destroy_all
 
 
-user1 = User.create(email: "toto@gmail.com", password: "azerty")
-user2 = User.create(email: "tata@gmail.com", password: "azerty")
-user3 = User.create(email: "tonton@gmail.com", password: "azerty")
-user4 = User.create(email: "titi@gmail.com", password: "azerty")
+user1 = User.create!(email: "toto@gmail.com", password: "azerty")
+user2 = User.create!(email: "tata@gmail.com", password: "azerty")
+user3 = User.create!(email: "tonton@gmail.com", password: "azerty")
+user4 = User.create!(email: "titi@gmail.com", password: "azerty")
 
-event1 = Event.create(name: "Noel des copains", user_id: user1.id)
-event2 = Event.create(name: "Galette des rois", user_id: user1.id)
-event3 = Event.create(name: "Halloween", user_id: user2.id)
-event4 = Event.create(name: "Surprise pour Jacques", user_id: user3.id)
-event5 = Event.create(name: "Fête de la musique", user_id: user3.id)
+event1 = Event.create!(name: "Noel des copains", address: "79 la Canebière", user_id: user1.id)
+event2 = Event.create!(name: "Galette des rois", address: "26 rue de la république", user_id: user1.id)
+event3 = Event.create!(name: "Halloween", address: "397 corniche kennedy", user_id: user2.id)
+event4 = Event.create!(name: "Surprise pour Jacques", address: "70 av du prado", user_id: user3.id)
+event5 = Event.create!(name: "Fête de la musique", address: "38 Quaie rive-neuve", user_id: user3.id)
 
 EventRegisteredUser.create(user_id: user4.id, event_id: event1.id)
 
