a645bf2dc733f2b0467114ac2ff435839b582d5b
nicolas belin <belin.nicolas89@gmail.com>
2024-12-12 12:38:26 +0100

    Formulaire pour choisir une date de l'évènement

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,10 @@ class Album < ApplicationRecord
   belongs_to :event
 
   has_many_attached :media
+
+  private
+
+  def set_default_name
+    self.name = "Album de l'évènement #{event.name}" unless name.present?
+  end
 end
--- app/models/monney_added.rb
+++ app/models/monney_added.rb
@@ -2,7 +2,8 @@ class MonneyAdded < ApplicationRecord
   belongs_to :user
   belongs_to :jackpot
 
-
+  private 
+  
   def total_amount
     monney_addeds.sum(:amount)
   end
