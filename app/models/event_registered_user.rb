58766b8081266fa8784c281ca7968cb232c7177e
stephanie mihut <stephanie.mihut@gmail.com>
2024-12-17 10:30:47 +0100

    fixed enum status in eventregistereduser model

--- app/models/event_registered_user.rb
+++ app/models/event_registered_user.rb
@@ -2,7 +2,4 @@ class EventRegisteredUser < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :status, inclusion: { in: ["pending", "accepted", "declined"] }
-
-  enum status: { pending: "En attente", accepted: "Accepté", declined: "Refusé" }
 end
