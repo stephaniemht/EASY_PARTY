d5028de452e1e15bfff16349c1fadbcf8b5d8def
Dylane Piro <piro.dylan@outlook.fr>
2024-12-18 14:25:29 +0100

    flatpickr almost finished and fixed things like avatar bug in events/show

--- app/javascript/controllers/calendar_show_controller.js
+++ app/javascript/controllers/calendar_show_controller.js
@@ -5,43 +5,57 @@ import flatpickr from 'flatpickr';
 export default class extends Controller {
   static targets = ["eventDate"]
   static values = {
-    dates: Array,
+    dates: Array,  // Array contenant les dates présélectionnées
   }
 
   connect() {
-    console.log(this.eventDateTargets);
-    console.log("coucou")
-
     flatpickr(this.element, {
       inline: true,
       defaultDate: this.datesValue,
-      onChange: (event) => {
-        document.querySelectorAll(".flatpickr-day.selected").forEach((dayElem) => {
-          const dayDate = dayElem.dateObj.toISOString().split("T")[0];
-          dayElem.setAttribute("data-calendar-show-target", this.datesValue);
-          dayElem.innerHTML = `<span>${dayElem.innerHTML}</span>`;
-          this.setEventListener(dayElem)
-        });
+      mode: "multiple",
+      onReady: () => {
+        this.updateDateStyles();
       },
+      onChange: () => {
+        this.updateDateStyles();
+      },
+    });
+  }
+
+
+  updateDateStyles() {
+    const allDates = document.querySelectorAll(".flatpickr-day");
+
+    const formattedDatesValue = this.datesValue.map((date) => {
+      const dateObject = new Date(date.split("T")[0]);
+      const options = { month: 'long', day: '2-digit' };
+      const formattedDate = new Intl.DateTimeFormat('en-EN', options).format(dateObject);
+      return formattedDate;
+    });
+
+
+    allDates.forEach((dayElem) => {
+      const dayDate = dayElem.ariaLabel.split(",")[0];
+      if (formattedDatesValue.includes(dayDate)) {
+        dayElem.classList.add("custom-selected");
+      } else {
+        dayElem.classList.remove("custom-selected");
+      }
+      this.setEventListener(dayElem);
     });
   }
 
   setEventListener(dayElem) {
     dayElem.addEventListener("click", (event) => {
-     const clickedDates = dayElem.ariaLabel.split(',')[0]
-      this.eventDateTargets.forEach(targetedDate => {
-        if (targetedDate.innerText.split(',')[0].trim() == clickedDates ) {
-          console.log("quelle histoire");
-          targetedDate.classList.remove('d-none')
-        } else {
-          targetedDate.classList.add('d-none')
-        }
-      })
-    })
-  }
+      const clickedDate = dayElem.ariaLabel.split(',')[0];
 
-  compare(event) {
-    console.log("coucou")
-    console.log(event.currentTarget)
+      this.eventDateTargets.forEach(targetedDate => {
+        if (targetedDate.innerText.split(',')[0].trim() === clickedDate) {
+          targetedDate.classList.remove('d-none');
+        } else {
+          targetedDate.classList.add('d-none');
+        }
+      });
+    });
   }
 }
